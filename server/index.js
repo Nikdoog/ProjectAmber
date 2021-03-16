0f98f66b3411bfecca8e8b8502f39fdcf18055e9
Shane Phillips <shanephillips@Shanes-MacBook-Pro.local>
2021-03-16 15:10:06 -0400

    Notes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
 
+// Navigation
+
 const clientPath = path.join(__dirname, '../client');
 const staticPath = path.join(clientPath, '/static');
 
+// Basic Server
+
 const app = express();
-
 app.use(express.static(staticPath));
-
 app.listen(1000);
