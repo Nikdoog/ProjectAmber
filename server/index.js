d766fb93e5d7bd3b3109981c2b0df126cd6fa510
Shane Phillips <shanephillips@Shanes-MacBook-Pro.local>
2021-03-23 13:56:20 -0400

    testing checklist material

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const path = require('path');
 
+// testing
+const dotenv = require('dotenv').config();
+const session = require('express-session');
+
+const port = process.env.PORT || 4000;
+
+const sessionSecret = process.env.SECRET || 'chips';
+
 // Navigation
 
 const clientPath = path.join(__dirname, '../client');
@@ -10,5 +18,18 @@ const staticPath = path.join(clientPath, '/static');
 
 const app = express();
 app.use(express.static(staticPath));
+
+// testing
+app.use(session({
+    cookie: {
+        maxAge: 1000*60*60*24*7,
+        secure: false
+    },
+    key: 'user_sid',
+    secret: sessionSecret,
+    resave: false,
+    saveUninitialized: false,
+    name: 'ProjectAmber'
+}));
 
 app.listen(4000);
